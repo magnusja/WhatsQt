c28401b2f1442ccab3474623874f15775a70a17f
jahnen <jahnen@in.tum.de>
2015-08-30 20:17:47 +0200

    Remove margin set window title chrome user agent

--- view/webview.cpp
+++ view/webview.cpp
@@ -2,13 +2,12 @@
 #include <QWebEnginePage>
 
 #include "webview.h"
-#include "net/cookiejar.h"
 
 WebView::WebView(QWidget *parent)
     : QWebEngineView(parent)
 {
     QWebEngineProfile *profile = new QWebEngineProfile(QLatin1String("WhatsQt"), this);
-    profile->setHttpUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12");
+    profile->setHttpUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/600.7.12");
     profile->setHttpCacheType(QWebEngineProfile::DiskHttpCache);
     profile->setPersistentCookiesPolicy(QWebEngineProfile::AllowPersistentCookies);
 
