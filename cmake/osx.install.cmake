110131e2df6b89df30927e7a5ce4e48cb6a33c89
jahnen <jahnen@in.tum.de>
2015-12-25 18:43:56 +0100

    Try to fix framework not found in app bundle when building with travis ci on OS X

--- cmake/osx.install.cmake
+++ cmake/osx.install.cmake
@@ -1,5 +1,5 @@
 # make sure that macdeployqt is exported in PATH!
-install(CODE "execute_process(COMMAND macdeployqt ${CMAKE_BINARY_DIR}/WhatsQt.app)")
+install(CODE "execute_process(COMMAND macdeployqt ${CMAKE_BINARY_DIR}/WhatsQt.app -executable=${CMAKE_BINARY_DIR}/WhatsQt.app/Contents/MacOS/)")
 install(TARGETS WhatsQt DESTINATION ./)
 
 set(CPACK_GENERATOR "DragNDrop")
