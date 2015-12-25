42333a10b8551fc9d9fc5ef4281b683c681c2ade
jahnen <jahnen@in.tum.de>
2015-12-25 18:50:24 +0100

    Correct usage of executable flag in macdeployqt executable flag forces deployment of QtWebEngineProcess

--- cmake/osx.install.cmake
+++ cmake/osx.install.cmake
@@ -1,5 +1,5 @@
 # make sure that macdeployqt is exported in PATH!
-install(CODE "execute_process(COMMAND macdeployqt ${CMAKE_BINARY_DIR}/WhatsQt.app -executable=${CMAKE_BINARY_DIR}/WhatsQt.app/Contents/MacOS/)")
+install(CODE "execute_process(COMMAND macdeployqt ${CMAKE_BINARY_DIR}/WhatsQt.app -executable=${CMAKE_BINARY_DIR}/WhatsQt.app/Contents/MacOS/WhatsQt)")
 install(TARGETS WhatsQt DESTINATION ./)
 
 set(CPACK_GENERATOR "DragNDrop")
