71a48db72a9d9a9f8997b67e278883f38520d251
jahnen <jahnen@in.tum.de>
2015-12-25 01:37:32 +0100

    Use cmake qt prefix

--- travis/compile.sh
+++ travis/compile.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+prefix=""
+if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+  prefix="-DCMAKE_PREFIX_PATH=$(echo /usr/local/Cellar/qt5/5.*/)"
+fi
+
 mkdir build
 cd build
 
-cmake ../
+cmake ../ $prefix
 make package -j2
