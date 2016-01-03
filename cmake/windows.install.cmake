4f71ef3bc6216d6b44b41edf332f899d7dd00447
jahnen <jahnen@in.tum.de>
2016-01-03 03:53:42 +0100

    Add Qt dependencies in nsis installer

--- cmake/windows.install.cmake
+++ cmake/windows.install.cmake
@@ -1,11 +1,15 @@
+
 # make sure that macdeploywin is exported in PATH!
 install(TARGETS WhatsQt DESTINATION bin)
-#install(CODE "execute_process(COMMAND windeployqt ${CMAKE_INSTALL_DIR}/bin/${PROJECT_NAME}.exe")
+install(CODE "execute_process(COMMAND windeployqt ${CMAKE_BINARY_DIR}/Release/${PROJECT_NAME}.exe --dir ${CMAKE_BINARY_DIR}/deps)")
+install(DIRECTORY ${CMAKE_BINARY_DIR}/deps/ DESTINATION bin)
+
+include(DeployQt4)
 
 set(CPACK_GENERATOR "NSIS")
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "${PROJECT_NAME}")
 set(CPACK_NSIS_MODIFY_PATH OFF)
-set(CPACK_NSIS_INSTALLED_ICON_NAME "${PROJECT_NAME}.exe")
+set(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\\\${PROJECT_NAME}.exe")
 set(CPACK_NSIS_MUI_FINISHPAGE_RUN "${PROJECT_NAME}.exe")
 set(CPACK_PACKAGE_EXECUTABLES ${PROJECT_NAME} ${PROJECT_NAME})
 set(CPACK_NSIS_URL_INFO_ABOUT "https://github.com/mjdev/WhatsQt")
