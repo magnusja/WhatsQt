86235a3a28541a1fdb6f94fd0a57716d53d87681
jahnen <jahnen@in.tum.de>
2016-01-04 00:29:34 +0100

    Install icon on linux

--- cmake/linux.install.cmake
+++ cmake/linux.install.cmake
@@ -3,8 +3,8 @@ configure_file(installer/linux/WhatsQt.desktop.in ${CMAKE_BINARY_DIR}/WhatsQt.de
 
 install(TARGETS WhatsQt DESTINATION bin)
 
-# TODO desktop file and icon
 install(FILES ${CMAKE_BINARY_DIR}/WhatsQt.desktop DESTINATION share/applications/)
+install(FILES artwork/icon/icon256.png DESTINATION share/icons/hicolor/256x256/apps RENAME WhatsQt.png)
 
 set(CPACK_GENERATOR "DEB;RPM")
 # TODO: add depends
