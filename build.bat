3df143fa0a4d2ee766b04b71a421c43ac18174ab
jahnen <jahnen@in.tum.de>
2016-01-03 02:17:40 +0100

    MSVC release

--- build.bat
+++ build.bat
@@ -5,4 +5,4 @@ set CMAKE_PREFIX_PATH=C:\Programme\Qt\5.5\msvc2013\lib\cmake
 mkdir build
 cd build
 cmake .. -G "Visual Studio 12 2013"
-msbuild ALL_BUILD.vcxproj
+msbuild ALL_BUILD.vcxproj /p:Configuration=Release
