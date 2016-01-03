9e8d31dcb43b2acc621ba24b5ed6d6781470c0ab
jahnen <jahnen@in.tum.de>
2016-01-04 00:23:57 +0100

    Add linux desktop file

--- cmake/linux.install.cmake
+++ cmake/linux.install.cmake
@@ -1,7 +1,10 @@
 
+configure_file(installer/linux/WhatsQt.desktop.in ${CMAKE_BINARY_DIR}/WhatsQt.desktop)
+
 install(TARGETS WhatsQt DESTINATION bin)
 
 # TODO desktop file and icon
+install(FILES ${CMAKE_BINARY_DIR}/WhatsQt.desktop DESTINATION share/applications/)
 
 set(CPACK_GENERATOR "DEB;RPM")
 # TODO: add depends
