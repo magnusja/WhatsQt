6e151069188497b4c1075a0a2e32cbff57943578
jahnen <jahnen@in.tum.de>
2016-01-03 03:09:14 +0100

    Add license to NSIS installer Create shortcuts

--- cmake/windows.install.cmake
+++ cmake/windows.install.cmake
@@ -1,7 +1,19 @@
 # make sure that macdeploywin is exported in PATH!
 install(TARGETS WhatsQt DESTINATION bin)
-#install(CODE "execute_process(COMMAND windeployqt ${CMAKE_INSTALL_DIR}/bin/WhatsQt.exe")
+#install(CODE "execute_process(COMMAND windeployqt ${CMAKE_INSTALL_DIR}/bin/${PROJECT_NAME}.exe")
 
 set(CPACK_GENERATOR "NSIS")
-set(CPACK_PACKAGE_INSTALL_DIRECTORY "WhatsQt")
-SET(CPACK_NSIS_MODIFY_PATH ON)
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "${PROJECT_NAME}")
+set(CPACK_NSIS_MODIFY_PATH OFF)
+set(CPACK_NSIS_INSTALLED_ICON_NAME "${PROJECT_NAME}.exe")
+set(CPACK_NSIS_MUI_FINISHPAGE_RUN "${PROJECT_NAME}.exe")
+set(CPACK_PACKAGE_EXECUTABLES ${PROJECT_NAME} ${PROJECT_NAME})
+set(CPACK_NSIS_URL_INFO_ABOUT "https://github.com/mjdev/WhatsQt")
+set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE")
+
+# Create the desktop link
+set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "  CreateShortCut '$DESKTOP\\\\${PROJECT_NAME}.lnk' '$INSTDIR\\\\bin\\\\${PROJECT_NAME}.exe' ")
+set( CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS "  
+    # Delete the Desktop link at uninstallation.
+    Delete '$DESKTOP\\\\${PROJECT_NAME}.lnk' ")
